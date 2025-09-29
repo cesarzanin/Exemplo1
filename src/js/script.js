d72769a31c205982e23950cf10a25b9453b7d932
César <zanincesar05@gmail.com>
2025-09-29 11:36:31 -0300

    Declarando variáveis no java

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
 alert("Hello World!")
+
+//{} LISTA PARA OBJETO
+//() LISTA ARRAY (PARA VÁRIOS OBJETOS)
+//[] LISTA DE MÉTODO (FUNÇÃO)
+
+//DECLARAÇÃO E VARÍAVEIS
+
+var nome = 'Fiap';
+console.log(typeof nome)
+
+let nome1 = 10;
+console.log(nome1);
+
+const nome2 = {}; //CONSTANTE QUE NÃO PODE SER ALTERADA
+console.log(nome2) 
+
+let nome3 = {}
+console.log(typeof nome3) //TYPEOF ME DIZ QUAL O TIPO DA LISTA NO INSPECT DA PÁGINA
+
+let nome4=true;
+console.log(typeof nome4)
